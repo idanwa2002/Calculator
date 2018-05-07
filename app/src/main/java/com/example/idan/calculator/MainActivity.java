a46aa324d25e69a69b0ef1463890fcf2d3dbce0f
idanwa2002 <av5307@bs.amalnet.k12.il>
2018-05-07 22:35:31 +0300

    try

--- app/src/main/java/com/example/idan/calculator/MainActivity.java
+++ app/src/main/java/com/example/idan/calculator/MainActivity.java
@@ -15,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     int op, sum = 0;
     String str;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
